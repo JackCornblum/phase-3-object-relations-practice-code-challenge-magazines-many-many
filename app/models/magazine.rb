83bfc8ccb6ff49323df41fd4cc4de435ed9a72dc
jackcornblum <jcornblum@gmail.com>
2021-07-08 10:15:40 -0400

    deliverables complete

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,17 @@
 class Magazine < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :readers, through: :subscriptions
+
+    def email_list
+        self.readers.map {|reader| reader.email}.join(";")
+    end
+
+    def self.most_popular
+       magazine_ids = Subscription.all.map {|subscription| subscription.magazine_id}
+
+       freq = magazine_ids.inject(Hash.new(0)) {|h,v| h[v] += 1; h}
+       id = magazine_ids.max_by {|v| freq[v]}
+       Magazine.find(id)
+    end
+
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,17 @@
 class Reader < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :magazines, through: :subscriptions
+
+    def subscribe(magazine, price)
+        Subscription.create(magazine_id: magazine.id, price: price, reader_id: self.id)
+    end
+
+    def total_subscription_price
+        self.subscriptions.map {|subscription| subscription.price }.sum
+    end
+
+    def cancel_subscription(magazine)
+        Subscription.where(magazine_id: magazine.id, reader_id: self.id).destroy_all
+    end
+
 end
